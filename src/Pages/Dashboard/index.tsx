0de4c462477c37e4ec2bec19167c9ef9ffbe0f1e
Dantts <gabrielteixeiradantas@hotmail.com>
2022-05-18 18:22:32 -0300

    feat: criando page de dashboard e component summary da aplicação

---
+++ src/Pages/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Summary } from '../../components/Summary';
+import { Container } from './styles';
+
+export const Dashboard = () => {
+  return (
+    <Container>
+      <Summary />
+    </Container>
+  );
+};
